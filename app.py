369737860e5916256329322c18bbee1d5f439deb
Ashu-Paytm <t-ashutosh4.jha@ocltp.com>
2025-04-03 16:11:30 +0530

    added the gunicorn functionality

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def receive_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    from gunicorn.app.wsgiapp import run
+    run()
